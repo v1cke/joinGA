67dec9fe82c7333f4bab88c5f7d85716a8cb1b6d
v1cke <v1cke1982@gmail.com>
2022-06-03 19:23:45 +0200

    addTaskRebuild

--- js/addtask.js
+++ js/addtask.js
@@ -1,27 +1,45 @@
-let tasks = [{
-    "title": "add Task erstellen",
-    "category": "todo",
-    "department": "management",
-    "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla perspiciatis, itaque atque et ut quasi. Deleniti ipsum facilis similique at.",
-    "date": "06.06.2022",
-    "urgency": "low",
-}, ];
+let tasks = [];
+let users = [];
+//let taskID = 0;
 
-function createTask() {
+
+/**
+ * function to put values of the task as a JSON in an array and save it in storage
+ * 
+ * @param {string} title - Value of the field title
+ * @param {category} category - Value of the field category
+ * @param {date} date - Value of the field date
+ * @param {urgency} urgency - Value of the field urgency
+ * @param {text} text - Value of the field description
+ * @param {id} id - id of the created task
+ * */
+async function createTask() {
     let title = document.getElementById('title').value;
-    let date = document.getElementById('date').value;
     let category = document.getElementById('category').value;
+    let date = document.getElementById('date').value;
     let urgency = document.getElementById('urgency').value;
     let text = document.getElementById('description').value;
+    //taskID = taskID ++;
+    generateTask(title, date, category, urgency, text);
+    backend.setItem('tasks', JSON.stringify(tasks));
+    confirmTask();
+}
 
+
+/**
+ * @param {task} task - creates new JSON with data from function createTask
+ * JSON task gets pushed into array tasks
+ * */
+function generateTask(title, date, category, urgency, text) {
     let task = {
-        title: title,
-        date: date,
-        category: category,
-        urgency: urgency,
-        text: text
+        'title': title,
+        'date': date,
+        'category': category,
+        'urgency': urgency,
+        'description': text,
+        //   'taskID': taskID,
+        'menu': false,
     }
-
     tasks.push(task);
 }
 
@@ -37,11 +55,18 @@ async function loadTasks() {
 }
 
 
+/**
+ * 
+ * @param {taskID} taskID - shows confirmMessage container with actual ID of the task, closes container after 3 seconds
+ */
+function confirmTask() {
+    //document.getElementById('TaskID').innerHTML = taskID;
+    document.getElementById('confirmMessage').style.display = "block";
 
-setTimeout(function() {
-    document.getElementById('confirmMessage').style.display = "none";
-}, 2000);
-
+    setTimeout(function() {
+        document.getElementById('confirmMessage').style.display = "none";
+    }, 2000);
+}
 
 
 function cancelTask() {
@@ -55,13 +80,13 @@ function chooseUser() {
     document.getElementById("chooseUserContainer").innerHTML = "";
     document.getElementById("chooseUserContainer").classList.remove("d-none");
     document.body.classList.add('overflow-hidden');
-    document.getElementById("chooseUserContainer").innerHTML += /*html*/ `
+        document.getElementById("chooseUserContainer").innerHTML += /*html*/ `
         <a id="closeBtn" href="#" onclick="closeUserContainer()"><img src="img/close.png" class="close-btn"></a>
         `;
     for (let i = 0; i < users.length; i++) {
         let userName = users[i]["name"];
         let userImg = users[i]["img"];
-
+        
         //add data to ChooseUserContainer
         document.getElementById("chooseUserContainer").innerHTML += /*html*/ `
         <div class="choose-person" onclick="addUser(${i}, '${userName}')">
@@ -72,10 +97,10 @@ function chooseUser() {
         </div>`;
     }
     //showCheckUp();
-}
+} 
 
 
 function closeUserContainer() {
-    document.getElementById("chooseUserContainer").classList.add("d-none");
-    document.body.classList.remove('overflow-hidden');
-}
+        document.getElementById("chooseUserContainer").classList.add("d-none");       
+        document.body.classList.remove('overflow-hidden');
+  }

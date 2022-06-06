bdeb249dace904035020b95725dae363d50ec028
v1cke <v1cke1982@gmail.com>
2022-06-06 12:22:04 +0200

    login and header img

--- js/start-side.js
+++ js/start-side.js
@@ -1,7 +1,22 @@
 let accountData = [];
+let loggedUser = [];
 
 async function loadTasks() {
     await downloadFromServer();
     accountData = JSON.parse(backend.getItem('user')) || [];
     console.log(accountData);
+}
+
+
+async function loginUser(){
+    let userName = document.getElementById('userNameField').value;
+    let password = document.getElementById('passwordField').value;
+    for (let i = 0; i < accountData.length; i++) {
+        const user = accountData[i];
+        if (userName == user.name && password == user.passwort){
+            loggedUser.push('user');
+            await backend.setItem('loggedUser', JSON.stringify(loggedUser));
+            window.location.href ="addtask.html";
+        } 
+    }
 }
